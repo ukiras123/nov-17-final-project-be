a8af855a1abc10fa61242e2507a4f626322bce8a
Kiran Gautam <ukiras@gmail.com>
2023-11-17 11:31:14 +1100

    adding eslint

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
-const express = require("express");
-require("dotenv").config();
-const cors = require("cors");
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+
 const app = express();
 
 const PORT = process.env.PORT || 3000;
@@ -9,15 +10,13 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
+app.get('/', (req, res) => {
   res.json({
-    status: "success",
-    message: "Server is live",
+    status: 'success',
+    message: 'Server is live',
   });
 });
 
-app.listen(PORT, (error) => {
-  error
-    ? console.log(error)
-    : console.log(`Server is running at http://localhost:${PORT}`);
-});
+app.listen(PORT, (error) => (error
+  ? console.log(error)
+  : console.log(`Server is running at http://localhost:${PORT}`)));
