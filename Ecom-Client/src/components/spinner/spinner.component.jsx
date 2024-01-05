56a95d15638239af7b47bd8b3ea540fe394f6d41
Kiran Gautam <ukiras@gmail.com>
2024-01-05 11:59:19 +1100

    delete project

--- Ecom-Client/src/components/spinner/spinner.component.jsx
+++
@@ -1,9 +0,0 @@
-import { SpinnerContainer, SpinnerOverlay } from './spinner.styles';
-
-const Spinner = () => (
-  <SpinnerOverlay data-testid='spinner'>
-    <SpinnerContainer />
-  </SpinnerOverlay>
-);
-
-export default Spinner;
