c3f4126d214fcb5bea04a8bf6d1aad958c37ca45
Pawan Kumar Siwakoti <pksiwako@deakin.edu.au>
2024-01-10 20:48:04 +1100

    auth error fixed and new features implemented

--- eCommerce-Client/api/src/model/client/ClientModel.js
+++ eCommerce-Client/api/src/model/client/ClientModel.js
@@ -1,53 +1,59 @@
 const mongoose = require("mongoose");
 
-const clientModel = new mongoose.Schema({
-    status:{
-        type: String,
-        default: "inactive"
+const clientModel = new mongoose.Schema(
+  {
+    status: {
+      type: String,
+      default: "inactive",
     },
-    fName:{
-        type: String,
-        required: true
+    fName: {
+      type: String,
+      required: true,
     },
-    lName:{
-        type: String,
-        required: true
+    lName: {
+      type: String,
+      required: true,
     },
-    address:{
-        type: String,
-        required: true
+    address: {
+      type: String,
+      required: true,
     },
-    phone:{
-        type: String,
-        required: true
+    phone: {
+      type: String,
+      required: true,
     },
-    email:{
-        type: String,
-        unique: true,
-        required: true
+    email: {
+      type: String,
+      unique: true,
+      required: true,
     },
-    password:{
-        type: String,
-        unique: true,
-        required: true
+    password: {
+      type: String,
+      unique: true,
+      required: true,
     },
     isverified: {
-        type: Boolean,
-        default: false,
+      type: Boolean,
+      default: false,
     },
     verificationCode: {
-        type: String,
-        default: '',
+      type: String,
+      default: "",
     },
-},
+    refreshJWT: {
+      type: String,
+      default: "",
+    },
+  },
 
-{ timestamps: true },
-)
+  { timestamps: true }
+);
 
-const Client = mongoose.model('client', clientModel);
+const Client = mongoose.model("client", clientModel);
 
-const createClient = (clientObj)=> Client.create(clientObj)
-const getClient = (filter) => Client.findOne(filter)
-const updateClient = (filter, updateObj) => Client.findOneAndUpdate(filter, updateObj)
+const createClient = (clientObj) => Client.create(clientObj);
+const getClient = (filter) => Client.findOne(filter);
+const updateClient = (filter, updateObj) =>
+  Client.findOneAndUpdate(filter, updateObj);
 
-module.exports = {createClient, getClient, updateClient}
+module.exports = { createClient, getClient, updateClient };
