21e7bb8bcb46d5263593e08e10ab3c078eb8f3a7
Nirajchemjong <nirajlimbu7@gmail.com>
2024-01-16 20:21:08 +1100

    clear sorry from niraj

--- eCommerce-Admin/api/config/mongoConfig.js
+++ eCommerce-Admin/api/config/mongoConfig.js
@@ -1,8 +1,8 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const mongoConnect = () => {
-  const DB_URL = process.env.DB_URL
-        || 'mongodb://127.0.0.1:27017/final_project';
+  const DB_URL =
+    process.env.DB_URL || "mongodb://127.0.0.1:27105/final_project";
 
   return mongoose.connect(DB_URL);
 };
